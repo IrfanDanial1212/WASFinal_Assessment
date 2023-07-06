c38e4014860b38c8f50eece5ed040a90fb4da72c
Irfan Danial <85051702+IrfanDanial1212@users.noreply.github.com>
2023-07-06 12:28:45 +0800

    Add files via upload
    Signed-off-by: Irfan Danial <85051702+IrfanDanial1212@users.noreply.github.com>

---
+++ WAS_FA_Enhanced/about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+        
+    <link rel="stylesheet" href="css/about.css">
+        
+</head>
+<style>
+h0 {
+	font-size: 60px;
+	}
+</style>
+<body>
+    <div class="header">
+        <img src="image/limaBL_Logo.png" alt="About Page" width="400" height="152"/></a>
+    </div>
+        
+        
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a class="active" href="about.php">About</a></li>
+        <li><a href="donate.php">Donate</a></li>
+        <li2><a href="cart.php">Cart</a></li2>
+    </ul>
+	<br><br>
+	<div class="row" style="background-color: #6DB9D8; ">
+        <div class="column left"></div>
+        <div class="column middle">
+            <p><center><b><h1>Who are we?</h1></b></center></p>       
+            <p><center><h1>We are 5 Budak Lapar and this is a website that sell fast food while pursuing sustainable development goal</h1></center></p>
+        </div></div>
+		
+	
+	<table border="0">
+            <tr>
+            <td style="background-color: #B07AED; ">
+                <h0><center><b>What are the</b></center></h0>
+				<h0><center><b>sustainable</b></center></h0>
+				<h0><center><b>development</b></center></h0>
+				<h0><center><b>goal that are</b></center></h0>
+				<h0><center><b>achieved?</b></center></h0>
+				<h2><center><u><i>Responsible consumption and reproduction</u></i></center></h2>
+                <h2><center><b>How?</b></center></h2>
+				<h2><center>Only use recycleable materials for foods' and drinks' packaging.</center></h2>
+            </td>
+			<td></td>
+            <td style="background-color: #B07AED; ">
+                <h2><center><u><i>Zero hunger</u></i></center></h2>
+                <h2><center><b>How?</b></center></h2>
+                <h2><center>By making an entire new page for the user to donate money that will be used to give foods for the needies.</center></h2>
+				<h2><center><u><i>No poverty</u></i></center></h2>
+				<h2><center><b>How?</b></center></h2>
+				<h2><center>If the amount that the customer needs to pay have cents, we will ask if the user wants to round the amount and the rounded amount will be used for donation.</center></h2>
+				<img src="image/sdg1.png" alt="About Page" width="180" height="180"/>
+				<img src="image/sdg2.png" alt="About Page" width="180" height="180"/>
+				<img src="image/sdg12.png" alt="About Page" width="180" height="180"/>
+            </td>
+            <td style="background-color: #E8E352; ">
+                <img src="image/allsdg.png" alt="About Page" width="500" height="300"/>
+				<h2><center><b>How to contact us?</b></center></h2>
+				<h2><center>Phone number : 017-7624183 or instagram : bLaparrr</center></h2>
+            </td>
+            </tr>
+    </table>
+	<br><br>
+		
+	<div class="footer">
+        <a href="https://www.instagram.com/">
+            <img src="image/f1.png" alt="" width="40" height="40"></a>
+        <a href="https://twitter.com/">
+            <img src="image/f2.png" alt="" width="40" height="40"></a>
+        <a href="https://web.whatsapp.com/">
+            <img src="image/f3.png" alt="" width="40" height="40"></a>
+    </div>
+</body>
+</html>
