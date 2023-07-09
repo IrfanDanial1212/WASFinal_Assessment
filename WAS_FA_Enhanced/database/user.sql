9eef02f42a89a0de46b2d856585aab8cf16f53dd
Irfan Danial <85051702+IrfanDanial1212@users.noreply.github.com>
2023-07-09 15:02:52 +0800

    Add files via upload
    Signed-off-by: Irfan Danial <85051702+IrfanDanial1212@users.noreply.github.com>

---
+++ WAS_FA_Enhanced/database/user.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2023 at 08:26 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webappsecfinal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `username` varchar(500) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`) VALUES
+(1, 'saiful', 'saiful', 'saiful@gmail.com', '@Bc12345'),
+(3, 'ikram', 'ikram', 'ikram@gmail.com', '@Bc12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ WAS_FA_Enhanced/menu pages/ala carte.php
@@ -0,0 +1,101 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/menu pages.css">
+    </head>
+
+    <body>
+        <div class="header">
+            <img src="../image/limaBL_Logo.png" alt="5BLlogo" width="400" height="152"></a>
+        </div>
+        <ul>
+            <li><a href="../home.php">Home</a></li>
+            <li><a class="active" href="../menu.php">Menu</a></li>
+            <li><a href="../about.php">About</a></li>
+            <li><a href="../donate.php">Donate</a></li>
+            <li><a href="../cart.php">Cart</a></li>
+        </ul>
+        <h1>
+            ALA CARTE
+        </h1>
+        <table>
+            <th scope="col">Ala Carte Name</th>
+            <th scope="col">Price</th>
+            <tr><td></td></tr>
+            <tr>
+                <td>
+                    Beef Burger Ala Carte
+                </td>
+                <td>
+                    RM 7.99
+                </td>
+            </tr>
+            <tr><td></td></tr>
+            <tr>
+                <td>
+                    Double Beef Burger Ala Carte
+                </td>
+                <td>
+                    RM 9.99
+                </td>
+            </tr>
+            <tr><td></td></tr>
+            <tr>
+                <td>
+                    Special Beef Burger Ala Carte
+                </td>
+                <td>
+                    RM 8.99
+                </td>
+            </tr>
+            <tr><td></td></tr>
+            <tr>
+                <td>
+                    Chicken Burger Ala Carte
+                </td>
+                <td>
+                    RM 5.99
+                </td>
+            </tr>
+            <tr><td></td></tr>
+            <tr>
+                <td>
+                    Double Chicken Burger Ala Carte
+                </td>
+                <td>
+                    RM 7.99
+                </td>
+            </tr>
+            <tr><td></td></tr>
+            <tr>
+                <td>
+                    Special Chicken Burger Ala Carte
+                </td>
+                <td>
+                    RM 6.99
+                </td>
+            </tr>
+        </table>
+
+        <div class="row">
+            <div class="column">
+                <img src="../image/Beef Burger.jpg" alt="Beef Burger" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="../image/Special Chicken Burger.jpg" alt="Special Chicken Burger" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="../image/Double Beef Burger.jpg" alt="Double Beef Burger" style="width:100%">
+            </div>
+        </div>
+
+        <ul class="navbar">
+            <li><a href="sets.php">Sets</a></li>
+            <li><a class="active" href="ala carte.php">Ala Carte</a></li>
+            <li><a href="beverages.php">Beverages</a></li>
+            <li><a href="desserts.php">Desserts</a></li>
+			
+
+        </ul>
+		
+    </body>
+</html>
